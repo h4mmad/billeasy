d6c0eca21b10e89c8249b49e6fdc0b2f831e8459
Hammad <mohammedhammad@gmail.com>
2025-06-06 00:40:50 +0530

    database_url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.DATABASE_URL) {
+  console.error("DATABASE_URL is not defined in environment variables");
+  process.exit(1);
+}
 
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/schemas/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    url:
-      process.env.DATABASE_URL ||
-      "postgresql://postgres.kcjggkubykawfittifyt:DOQt8Od9OxJW0DD0@aws-0-ap-south-1.pooler.supabase.com:6543/postgres",
+    url: process.env.DATABASE_URL!,
   },
 });
